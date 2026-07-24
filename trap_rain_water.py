8ec2fb469e0fe69498931fcc6d8da307619a4049
dylan <luodaojun1128@gmail.com>
2026-07-24 21:40:32 +0800

    add the two pass solution for trap water; working on the stack solution

--- trap_rain_water.py
+++ trap_rain_water.py
@@ -3,7 +3,7 @@ from typing import List
 
 class Solution:
     def trap(self, height: List[int]) -> int:
-        return self.trap_twoPass(height)
+        return self.trap_stack(height)
 
     def trap_bruteForce(self, height: List[int]) -> int:
         total = 0
@@ -31,12 +31,75 @@ class Solution:
             max_right = max(max_right, height[j + 1])
             max_right_list[j] = max_right
 
-        can_hold = [min(l, r) for l, r in zip(max_left_list, max_right_list)]
+        print(max_left_list)
+        print(max_right_list)
+        print(height)
+        can_hold = [
+            max(min(l, r) - h, 0)
+            for l, r, h in zip(max_left_list, max_right_list, height)
+        ]
         total = sum(can_hold)
 
         return total
 
+    def trap_stack(self, height: List[int]) -> int:
+        total = 0
+        stack = []
 
+        for i in range(len(height) - 1):
+            if height[i] > height[i + 1] or i == 0:
+                stack.append(i)
+            else:
+                left_wall = stack[-1]
+                right_wall = i + 1
+                floor = i
+                print(
+                    "Left wall index ",
+                    left_wall,
+                    " value ",
+                    height[left_wall],
+                    " right wall index ",
+                    i + 1,
+                    " value ",
+                    height[i + 1],
+                    " floor wall index ",
+                    i,
+                    " value ",
+                    height[i],
+                )
+                total += max(
+                    min(height[right_wall], height[left_wall]) - height[floor], 0
+                ) * (right_wall - floor)
+                print(total)
+
+                while len(stack) > 1:
+                    floor = stack.pop()
+                    left_wall = stack[-1]
+                    print(
+                        "Left wall index ",
+                        left_wall,
+                        " value ",
+                        height[left_wall],
+                        " right wall index ",
+                        i + 1,
+                        " value ",
+                        height[i + 1],
+                        " floor wall index ",
+                        i,
+                        " value ",
+                        height[i],
+                    )
+                    total += max(
+                        min(height[right_wall], height[left_wall]) - height[floor], 0
+                    ) * (right_wall - floor)
+                    print(total)
+        return total
+
+
+solution = Solution()
+print(solution.trap([3, 1, 0, 2]), " ", 3)
+
+print("------------------------")
 solution = Solution()
 print(solution.trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]), " ", 6)
 
