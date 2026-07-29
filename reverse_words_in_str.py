b6abb0d9b93453469e00829f57ec9b052805b6fe
dylan <luodaojun1128@gmail.com>
2026-07-29 12:51:17 +0800

    added reverser words in string leetcode and solved it

---
+++ reverse_words_in_str.py
@@ -0,0 +1,43 @@
+import math
+
+
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        return self.convert_simulation(s, numRows)
+
+    def convert_simulation(self, s: str, numRows: int) -> str:
+        output = ""
+        if numRows == 1:
+            return s
+        for row in range(numRows):
+            # first row, no middle column concerns
+            if row == 0:
+                i = 0
+                while i <= len(s) - 1:
+                    output += s[i]
+                    # top and botton row has no mid column
+                    i += numRows + (numRows - 2)
+            # last row, no middle column concerns
+            elif row == numRows - 1:
+                j = numRows - 1
+                while j <= len(s) - 1:
+                    output += s[j]
+                    j += numRows + (numRows - 2)
+            # middle rows, we start to consider mid column
+            else:
+                k = row
+                while k <= len(s) - 1:
+                    col_start = math.floor(k / (numRows + numRows - 2)) * (
+                        numRows + numRows - 2
+                    )
+                    next_col_start = col_start + (numRows + numRows - 2)
+                    output += s[k]
+                    # now let's add in middle column, the index of which is
+                    # the result of column start sum - k
+
+                    # the if is to protect the case where next middle column is empty
+                    if col_start + next_col_start - k <= len(s) - 1:
+                        output += s[col_start + next_col_start - k]
+                    k += numRows + (numRows - 2)
+        return output
+
