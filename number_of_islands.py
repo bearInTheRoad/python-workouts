e79a90d0e489296490d03c4d9b278bb4fcd310e7
dylan <luodaojun1128@gmail.com>
2026-08-30 16:14:25 +0800

    number_of_islands added dfs solution

--- number_of_islands.py
+++ number_of_islands.py
@@ -4,7 +4,7 @@ from collections import deque, defaultdict
 
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        return self.numIslands_bfs(grid)
+        return self.numIslands_dfs(grid)
 
     def belongIslands(self, grid, row, column, islands, island_counts):
 
@@ -94,6 +94,30 @@ class Solution:
                 # print("after ", visited)
 
         return islands_count
+
+    def numIslands_dfs(self, grid):
+        if not grid:
+            return 0
+
+        num_islands = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == "1":
+                    self.dfs(grid, i, j)
+                    num_islands += 1
+
+        return num_islands
+
+    def dfs(self, grid, r, c):
+        if r < 0 or c < 0 or c >= len(grid[0]) or r >= len(grid) or grid[r][c] == "0":
+            return
+
+        grid[r][c] = "0"
+
+        self.dfs(grid, r - 1, c)
+        self.dfs(grid, r + 1, c)
+        self.dfs(grid, r, c - 1)
+        self.dfs(grid, r, c + 1)
 
 
 solution = Solution()
